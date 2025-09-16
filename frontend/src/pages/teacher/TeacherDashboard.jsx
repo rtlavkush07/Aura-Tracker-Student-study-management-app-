5f31b866caf110144d02dc89e51ab14ca41d4f91
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2025-09-16 15:24:30 +0530

    Update TeacherDashboard.jsx

--- frontend/src/pages/teacher/TeacherDashboard.jsx
+++ frontend/src/pages/teacher/TeacherDashboard.jsx
@@ -12,7 +12,7 @@ const TeacherDashboard = () => {
   useEffect(() => {
     const fetchTeacherProfile = async () => {
       try {
-        const response = await axios.get("/api/user/teacher/profile", {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/user/teacher/profile`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
