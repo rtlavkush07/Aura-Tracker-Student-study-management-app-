2e30ca4fa40729b4c361a6d3654b21ba14484121
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2025-09-16 15:26:42 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   useEffect(() => {
     const fetchProfileData = async () => {
       try {
-        const response = await axios.get("/api/user/profile", {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/user/profile`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         if (response.data && response.data.userProfile) {
